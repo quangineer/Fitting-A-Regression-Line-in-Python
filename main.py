e1465c3046d23f05147f784b9395c785cecfab2b
Quan Nguyen <quangineer@gmail.com>
2019-07-08 22:48:55 -0700

    carats

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import numpy as np
 import statsmodels.api as sm 
 
 df = pd.read_csv("house_price_area_only.csv")
-df.head()
+# print (df.head())
+
+df['intercept'] = 1
+
+ln = sm.OLS(df["price"], df[['intercept', 'area']])
+results = ln.fit()
+print (results.summary())
